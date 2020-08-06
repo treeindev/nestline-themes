1ddbb9b36aa0b6f05481555cc35807eb6d6ced3c
treeindev <miquel@treeindev.net>
2020-08-06 23:26:51 +0200

    Refactor folder structure to accomodate new themes

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,25 @@ const path = require('path');
 
 module.exports = {
 	mode: 'production',
-	entry: [
-		'./src/styles/index.scss', 
-	],
+	entry: {
+        default: './src/default/default.scss',
+        cherry: './src/cherry/cherry.scss',
+        dark: './src/dark/dark.scss'
+    },
     output: {
-        path: path.resolve( __dirname, 'dist')
+        path: path.resolve( __dirname, 'dist'),
+        filename: 'js/webpack/[name].js'
     },
     module: {
         rules: [
             {
-				test: /\.scss$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: 'css/main.min.css',
-						}
-					},
-					{
-						loader: 'sass-loader'
-					}
-				]
+                test: /\.scss$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: 'css/[name].min.css'
+                    }
+                }, 'sass-loader']
 			}
         ]
     }
